cf5e7200eca189e4d6874a6d01cb07c9d6446996
feebrian <adamaditama13@gmail.com>
2024-01-06 12:04:19 +0700

    update mail transport property

--- src/MailSender.js
+++ src/MailSender.js
@@ -5,6 +5,7 @@ class MailSender {
     this._transport = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
